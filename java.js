c5d7a9e5609932534cfdb80d02d1b6e1c87a799f
kirstenmyette <kirstenmyette@aol.com>
2017-09-28 16:40:07 -0500

    completed lab

--- java.js
+++ java.js
@@ -1,24 +1,60 @@
-var friends = ["Matt:", "Josh:", "January:", "Brandi:", "Daniel:"];
-var friendsArray= ["Matt", "Josh", "January", "Brandi", "Daniel"]
+var friendsArray = ["Matt", "Josh", "January", "Brandi", "Daniel"]
+
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.createElement("button");
+    button.innerText = "Sing!";
+    button.addEventListener("click", function () {
+        for (var i = 0; i < friendsArray.length; i++) {
+            //console.log(friendsArray[i]);
+            var div = document.createElement("div");
+            div.className ="friend";
+            var h3 = document.createElement("h3");
+            var text= document.createTextNode(friendsArray[i]);
+            h3.appendChild(text);
+            div.appendChild(h3);
+            document.body.appendChild(div);
+         
+            
+         
+
+            for (j = 99; j > 0; j--) {         
+            if (j === 2) { 
+                var paragraph = document.createElement('p'); 
+                var text = document.createTextNode(j + " lines of code in the file, " + j + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
+                paragraph.appendChild(text);
+                div.appendChild(paragraph);
+            }
+            else if (j === 1){
+                var paragraph = document.createElement('p');
+                var text = document.createTextNode(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out,  no more lines of code in the file" );
+                paragraph.appendChild(text);
+                div.appendChild(paragraph);
+
+               
+            }
+            else {
+                var paragraph = document.createElement("p");
+                var text = document.createTextNode(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file" )
+                paragraph.appendChild(text);
+                div.appendChild(text);
+            
+            }
 
 
 
-for (var i = 0; i < friends.length; i++) {
-    console.log(friends[i]);
-
-    for (j = 99; j > 0; j--) { 
 
 
-    if (j === 2) {
-        console.log(j + " lines of code in the file, " + j + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
-    }
-    else if (j === 1){
-        console.log(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out,  no more lines of code in the file");
-    }
-    else {
-        console.log(j + " line of code in the file, " + j + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
-    }
-    
-    }
+        
+        }
+    });
+    document.body.appendChild(button);
+});
 
-}
+
+
+
+
+
+
+
+
